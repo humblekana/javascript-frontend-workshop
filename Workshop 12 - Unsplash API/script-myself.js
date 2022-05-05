4beefed992b22cf0ea313c04b6dfcff4b405e190
beta <lnbtkana2326@gmail.com>
2022-05-05 10:16:11 +0700

    workshop12

--- Workshop 12 - Unsplash API/script-myself.js
+++ Workshop 12 - Unsplash API/script-myself.js
@@ -1,11 +1,13 @@
+// ตัวแปรเก็บค่าว่าจะแสดงกี่รูป
 const count = 10;
 const apiKey = "CQbCQ8q232MegKTx520_bst_6hPFqpGitd0jQG8MCJ8"
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}` 
 
 const img_containerEl = document.getElementById("img-container")
-let pictureData
+let pictureData // ตัวแปรเก็บค่าที่ได้จาก การดึงAPI เป็น array
 
 async function getPhotos(){
+    // อาจจะใช้ try ก็ได้
     /*try{
         const response = await fetch(apiUrl)
         pictureData = await response.json()
@@ -14,12 +16,14 @@ async function getPhotos(){
     }catch(err){
         console.log(err)
     }*/
+    // ทำการ ดึงข้อมูล API ปกติ
     const response = await fetch(apiUrl)
     pictureData = await response.json()
     //console.log(pictureData)
     displayImage();
 }
 
+//ฟังก์ชันแสดงผลของภาพ
 function displayImage(){
     pictureData.forEach((photo)=>{
         console.log(photo)
@@ -37,5 +41,20 @@ function displayImage(){
     })
 }
 
+//เช็คการ scroll
+window.addEventListener("scroll",()=>{
+    //console.log(window.innerHeight)
+    //console.log(window.scrollY)
+    console.log(document.body.offsetHeight-100)
+    //console.log(window.innerWidth)
+    // window.innerHeight ความสูงของ window คงที่
+    // window.scrollY ขนาด scrollY เพิ่มขึ้นเรื่อยๆดักจับระยะ
+    // document.body.offsetHeight ความสูงของbody ขณะนั้น
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight-100){
+        //ดึงภาพมาแสดงผลในเว็บบราวเซอร์
+        getPhotos()
+    }
+})
 
+//ดึงภาพมาแสดงครั้งแรก
 getPhotos()
